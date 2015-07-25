48ba116502a9a59fbdc342773d53f707cc2919f6
LyneLiu <595596226@qq.com>
2015-07-25 22:34:01 +0800

    add erlang vm

---
+++ otp_src_17.5/erts/emulator/i686-pc-linux-gnu/erl_version.h
@@ -0,0 +1,6 @@
+/* This file was created by 'make_version' -- don't modify. */
+#define ERLANG_OTP_RELEASE "17"
+#define ERLANG_OTP_VERSION "17.5"
+#define ERLANG_VERSION "6.4"
+#define ERLANG_COMPILE_DATE "Sat Jul 25 13:17:52 2015"
+#define ERLANG_ARCHITECTURE "i686-pc-linux-gnu"
